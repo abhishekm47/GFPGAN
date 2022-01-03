39d063749433b38d98c75740b052934ae8bc80f6
Abhishek <abhishek121meena121@gmail.com>
2022-01-03 14:05:13 +0000

    second stage inference script for DubSync

--- inference.py
+++ inference.py
@@ -9,34 +9,11 @@ from basicsr.utils import imwrite
 from gfpgan import GFPGANer
 
 
-def main():
-    """Inference demo for GFPGAN.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--upscale', type=int, default=1, help='The final upsampling scale of the image')
-    parser.add_argument('--arch', type=str, default='clean', help='The GFPGAN architecture. Option: clean | original')
-    parser.add_argument('--channel', type=int, default=2, help='Channel multiplier for large networks of StyleGAN2')
-    parser.add_argument('--model_path', type=str, default='experiments/pretrained_models/GFPGANCleanv1-NoCE-C2.pth')
-    parser.add_argument('--bg_upsampler', type=str, default='realesrgan', help='background upsampler')
-    parser.add_argument(
-        '--bg_tile', type=int, default=400, help='Tile size for background sampler, 0 for no tile during testing')
-    parser.add_argument('--test_path', type=str, default='inputs/whole_imgs', help='Input folder')
-    parser.add_argument('--suffix', type=str, default=None, help='Suffix of the restored faces')
-    parser.add_argument('--only_center_face', action='store_true', help='Only restore the center face')
-    parser.add_argument('--aligned', action='store_true', help='Input are aligned faces')
-    parser.add_argument('--paste_back', action='store_false', help='Paste the restored faces back to images')
-    parser.add_argument('--save_root', type=str, default='results', help='Path to save root')
-    parser.add_argument(
-        '--ext',
-        type=str,
-        default='auto',
-        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
-    args = parser.parse_args()
+def gfpgan_sr_forward(img_list, save_root, args, options):
 
-    args = parser.parse_args()
-    if args.test_path.endswith('/'):
-        args.test_path = args.test_path[:-1]
-    os.makedirs(args.save_root, exist_ok=True)
+    if not os.path.exists(save_root):
+        os.makedirs(save_root, 0o777)
+    
 
     # background upsampler
     if args.bg_upsampler == 'realesrgan':
@@ -60,23 +37,30 @@ def main():
     else:
         bg_upsampler = None
     # set up GFPGAN restorer
-    restorer = GFPGANer(
+    
+    if options['second_stage_custom_checkpoint']:
+        restorer = GFPGANer(
+            model_path=options['second_stage_custom_checkpoint'],
+            upscale=args.upscale,
+            arch=args.arch,
+            channel_multiplier=args.channel,
+            bg_upsampler=bg_upsampler)
+    
+    else:
+        restorer = GFPGANer(
         model_path=args.model_path,
         upscale=args.upscale,
         arch=args.arch,
         channel_multiplier=args.channel,
         bg_upsampler=bg_upsampler)
 
-    img_list = sorted(glob.glob(os.path.join(args.test_path, '*')))
     for img_path in img_list:
         # read image
         img_name = os.path.basename(img_path)
         print(f'Processing {img_name} ...')
         basename, ext = os.path.splitext(img_name)
         input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        print('has_aligned:{}'.format(args.aligned))
-        print('only_center_face:{}'.format(args.only_center_face))
-        print('paste_back:{}'.format(args.paste_back))
+
         # restore faces and background if necessary
         cropped_faces, restored_faces, restored_img = restorer.enhance(
             input_img, has_aligned=args.aligned, only_center_face=args.only_center_face, paste_back=args.paste_back)
@@ -84,18 +68,18 @@ def main():
         # save faces
         for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
             # save cropped face
-            save_crop_path = os.path.join(args.save_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
+            save_crop_path = os.path.join(save_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
             imwrite(cropped_face, save_crop_path)
             # save restored face
             if args.suffix is not None:
                 save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
             else:
                 save_face_name = f'{basename}_{idx:02d}.png'
-            save_restore_path = os.path.join(args.save_root, 'restored_faces', save_face_name)
+            save_restore_path = os.path.join(save_root, 'restored_faces', save_face_name)
             imwrite(restored_face, save_restore_path)
             # save comparison image
             cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
-            imwrite(cmp_img, os.path.join(args.save_root, 'cmp', f'{basename}_{idx:02d}.png'))
+            imwrite(cmp_img, os.path.join(save_root, 'cmp', f'{basename}_{idx:02d}.png'))
 
         # save restored img
         if restored_img is not None:
@@ -105,14 +89,13 @@ def main():
                 extension = args.ext
 
             if args.suffix is not None:
-                save_restore_path = os.path.join(args.save_root,
+                save_restore_path = os.path.join(save_root,
                                                  f'{basename}_{args.suffix}.{extension}')
             else:
-                save_restore_path = os.path.join(args.save_root, f'{basename}.{extension}')
+                save_restore_path = os.path.join(save_root, f'{basename}.{extension}')
             imwrite(restored_img, save_restore_path)
 
-    print(f'Results are in the [{args.save_root}] folder.')
+    print(f'Results are in the [{save_root}] folder.')
 
 
-if __name__ == '__main__':
-    main()
+
